01e87501b07e18690757bd68d5b21f91ea4598ba
B0K1NG <dominykaspavlijus@gmail.com>
2025-01-08 15:18:40 +0200

    Update boolean values to immediately return a value.

--- Beginner/Day2/ghost-gobble-arcade-game/arcade_game.py
+++ Beginner/Day2/ghost-gobble-arcade-game/arcade_game.py
@@ -7,10 +7,7 @@ def eat_ghost(power_pellet_active, touching_ghost):
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - can a ghost be eaten?
     """
-    if power_pellet_active and touching_ghost:
-        return True
-    else:
-        return False
+    return power_pellet_active and touching_ghost
 
 
 def score(touching_power_pellet, touching_dot):
@@ -20,11 +17,7 @@ def score(touching_power_pellet, touching_dot):
     :param touching_dot: bool - is the player touching a dot?
     :return: bool - has the player scored or not?
     """
-
-    if touching_power_pellet or touching_dot:
-        return True
-    else:
-        return False
+    return touching_power_pellet or touching_dot
 
 
 def lose(power_pellet_active, touching_ghost):
@@ -34,12 +27,7 @@ def lose(power_pellet_active, touching_ghost):
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - has the player lost the game?
     """
-
-    if not power_pellet_active and touching_ghost:
-        return True
-    else:
-        return False
-    
+    return not power_pellet_active and touching_ghost
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
@@ -50,8 +38,4 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - has the player won the game?
     """
-
-    if has_eaten_all_dots and not lose(power_pellet_active, touching_ghost):
-        return True
-    else:
-        return False
+    return has_eaten_all_dots and not lose(power_pellet_active, touching_ghost)
